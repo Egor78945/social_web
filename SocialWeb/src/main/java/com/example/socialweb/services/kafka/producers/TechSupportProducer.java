6134526e298c75c05d96ab3b47ab0eb70442876d
Egor Petrov <Egor78396@gmail.com>
2024-03-31 13:04:20 +0300

    SocialWeb(2 microservices + remade producer and kafka config | 31.03.24)

--- SocialWeb/src/main/java/com/example/socialweb/services/kafka/producers/TechSupportProducer.java
+++ SocialWeb/src/main/java/com/example/socialweb/services/kafka/producers/TechSupportProducer.java
@@ -3,6 +3,7 @@ package com.example.socialweb.services.kafka.producers;
 import com.example.socialweb.models.requestModels.TechSupportRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -10,17 +11,18 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class TechSupportProducer {
     private final KafkaTemplate<String, TechSupportRequest> kafkaTemplate;
+    private final String topic;
+
+    public TechSupportProducer(KafkaTemplate<String, TechSupportRequest> kafkaTemplate, @Value("${kafka.topic}") String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
 
     public void send(TechSupportRequest request){
-        Message<TechSupportRequest> message = MessageBuilder
-                .withPayload(request)
-                .setHeader(KafkaHeaders.TOPIC, "techsupport")
-                .build();
-        kafkaTemplate.send(message);
+        kafkaTemplate.send(topic, request);
         log.info(String.format("Message sent -> %s", request.getMessage()));
     }
 }
